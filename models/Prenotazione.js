94f8fdb9fecf9dece2948d6ece93be678e94bb62
Forziere <73720066+forziere@users.noreply.github.com>
2025-06-22 18:18:42 +0200

    Update Prenotazione.js

--- models/Prenotazione.js
+++ models/Prenotazione.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const prenotazioneSchema = new mongoose.Schema({
+  utente: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   campo: { type: mongoose.Schema.Types.ObjectId, ref: 'Campo' },
-  giocatori: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  orario: String,
-  data: Date
+  data: { type: Date, required: true },
+  pagato: { type: Boolean, default: false }
 });
 
 export default mongoose.model('Prenotazione', prenotazioneSchema);
